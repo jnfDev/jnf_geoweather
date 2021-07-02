882fe0f854ecbd58e4a6e42929152d19670948af
jnfDev <jairo.jnf.2010@gmail.com>
2021-07-02 09:08:28 -0400

    Added Admin configuration page.

--- jnf_geoweather.php
+++ jnf_geoweather.php
@@ -115,4 +115,91 @@ class Jnf_Geoweather extends Module
     }
 
     /** Admin Configuration Page */
+
+    public function getContent()
+    {
+        $output = '';
+        $errors  = array();
+
+        if (Tools::isSubmit('submit'.$this->name)) {
+
+            $apiKey = strval(Tools::getValue('JNF_GEOWEATHER_API_KEY'));
+
+            if ( empty( $apiKey) ) {
+                $errors[] = $this->trans('API Key is required', [], 'Modules.Jnfimporter.Jnfimporter');
+            }
+        
+            if (!count($errors)) {
+                Configuration::updateValue('JNF_GEOWEATHER_API_KEY', $apiKey);
+                $output .= $this->displayConfirmation($this->trans('Successfully Updated', [], 'Modules.'));
+            } else {
+                $output .= $this->displayError(implode('<br />', $errors));
+            }
+        }
+
+
+        
+        return $output . $this->displayForm();
+    }
+
+    public function displayForm()
+    {
+        // Get default language
+        $defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+        // Init Fields form array
+        $fieldsForm[0]['form'] = [
+            'legend' => [
+                'title' => $this->trans('Geo Weather', [], 'Modules.Jnfwelcometext.Jnfwelcometext'),
+            ],
+            'input' => [
+                [
+                    'type'  => 'text',
+                    'label' => $this->trans('Weather API Key', [], 'Modules.Jnfwelcometext.Jnfwelcometext'),
+                    'name'  => 'JNF_GEOWEATHER_API_KEY',
+                    'required' => true,
+                    'col'  => 5
+                ],
+            ],
+            'submit' => [
+                'title' => $this->trans('Save', [], 'Modules.Jnfwelcometext.Jnfwelcometext'),
+                'class' => 'btn btn-default pull-right'
+            ]
+        ];
+
+        $helper = new HelperForm();
+
+        // Module, token and currentIndex
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+        // Language
+        $helper->default_form_language = $defaultLang;
+        $helper->allow_employee_form_lang = $defaultLang;
+
+        // Title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->toolbar_btn = [
+            'save' => [
+                'desc' => $this->trans('Save', [], 'Modules.Jnfwelcometext.Jnfwelcometext'),
+                'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                '&token='.Tools::getAdminTokenLite('AdminModules'),
+            ],
+            'back' => [
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->trans('Back to list', [], 'Modules.Jnfwelcometext.Jnfwelcometext')
+            ]
+        ];
+
+        // Load current value0
+        $helper->fields_value['JNF_GEOWEATHER_API_KEY'] = Tools::getValue('JNF_GEOWEATHER_API_KEY', Configuration::get('JNF_GEOWEATHER_API_KEY'));
+
+        return $helper->generateForm($fieldsForm);
+    }
+
 }
